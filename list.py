4dd3130f23077d5e58ffef542de0cbd779573096
rohanrajleo <bhoirohanraj@gmail.com>
2024-03-09 20:53:27 +0000

    all the IDE sucks

---
+++ list.py
@@ -0,0 +1,7 @@
+x = ["apple","pineapple","lemon"]
+x.append("orange")
+x.insert(1,"guava")
+if "orange" in x:
+  print (len(x))
+for items in x:
+  print (items+" yes") #prints it indivudually
---
+++ str.py
@@ -0,0 +1,5 @@
+x = "this is just a string"
+print (x)
+print (x[-1])
+print (x[2:8])
+
