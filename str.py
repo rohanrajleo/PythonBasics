93d5e3e384aa4dc627de6bacea844371a13197b4
Rohan <146360473+rohanrajleo@users.noreply.github.com>
2024-03-10 07:46:20 +0000

    functioins

--- str.py
+++ str.py
@@ -1,5 +1,26 @@
-x = "this is just a string"
-print (x)
-print (x[-1])
-print (x[2:8])
+def main() :
+  x = "this is just a string"
+  print (x)
+  print (x[-1])
+  print (x[2:10])
+  print (jo(10,10,"rectangle")) #default value function
+  pattern (4) #function
+def jo (b,h,shape='trianle'):
+  if (shape == 'rectangle'):
+      return b*h
+  return 0.5*b*h
+
+def pattern (n):
+   for i in range(n):
+      s = ''
+      for j in range(i+1):
+          s = s + '*'
+      print(s)
+      '''for i in range(1,n+1):
+            for j in range (i):
+               print ("*",end=" ")
+            print ("\n")'''
+
+if __name__ =="__main__":
+  main ()
 
