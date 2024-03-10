228dc9b5999879013b5302fb72b883ea3250e836
Rohan <146360473+rohanrajleo@users.noreply.github.com>
2024-03-10 10:28:06 +0000

    dict but value as a list

--- dict_tuples.py
+++ dict_tuples.py
@@ -1,10 +1,11 @@
-stocks = {"infosys": [600,630,620], "rilence": [1430,1490,1567],"tata" : [234,180,160]}    #dict but value is a list
+stocks = {"infosys": [600,630,620], "rilence": [1430,1490,1567],
+          "tata" : [234,180,160]}    #dict but value is a list
 prompt = input ("ENTER : ")
 if prompt=="print":
     for k,v in stocks.items():
         print (f"{k}==>{v}")
 elif prompt == "add":
-    company = input ("Enter Company Name : ")
+    company = input ("Enter Company Name : ").lower()
     price = int (input ("Enter stock price : "))
     if company in stocks:
         stocks[company].append(price)
