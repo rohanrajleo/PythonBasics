5b7870106446dea8d8d0f17cc4dd6b7610febb20
Rohan <146360473+rohanrajleo@users.noreply.github.com>
2024-03-12 13:50:30 +0000

    it is related to the class itself and no obj is created

--- classmethod.py
+++ classmethod.py
@@ -11,5 +11,5 @@ MagicalHat.assign("Harry")
 
 
 '''
-@classmethod takes the first input as cls, and functions defined with @classmethods are tied to the class itself, rather than its instances,they can excess modify class-level attributes and call other methods too
+@classmethod takes the first input as cls, and functions defined with @classmethods are tied to the class itself, rather than its instances,they can excess modify class-level attributes and call other methods too : it is used when when we do not need any obj of the class
 '''
